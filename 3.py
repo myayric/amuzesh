c5ff1f42251389b5c46570a364e6045cd08fb191
70680 <7068082@gmail.com>
2023-01-17 16:10:16 +0300

    погода

--- 3.py
+++ 3.py
@@ -1,10 +1,15 @@
+import now as now
 from bs4 import BeautifulSoup as bs
 import requests
+from datetime import datetime
+
 
 USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 " \
              "Safari/537.36 "
 # US english
 LANGUAGE = "ru-RU,ru;q=0.5"
+now1 = datetime.now()
+t = now1.strftime("%H:%M")
 
 
 def get_weather_data(url):
@@ -15,23 +20,14 @@ def get_weather_data(url):
     html = session.get(url)
     # create a new soup
     soup = bs(html.text, "html.parser")
-    # store all results on this dictionary
-    result = {}
-    # extract region
-    result['region'] = soup.find("div", attrs={"id": "wob_loc"}).text
-    # extract temperature now
-    result['temp_now'] = soup.find("span", attrs={"id": "wob_tm"}).text
-    # get the day and hour now
-    result['dayhour'] = soup.find("div", attrs={"id": "wob_dts"}).text
-    # get the actual weather
-    result['weather_now'] = soup.find("span", attrs={"id": "wob_dc"}).text
-    # get the precipitation
-    result['precipitation'] = soup.find("span", attrs={"id": "wob_pp"}).text
-    # get the % of humidity
-    result['humidity'] = soup.find("span", attrs={"id": "wob_hm"}).text
-    # extract the wind
-    result['wind'] = soup.find("span", attrs={"id": "wob_ws"}).text
-    # get next few days' weather
+    result = {'region': soup.find("div", attrs={"id": "wob_loc"}).text,
+              'temp_now': soup.find("span", attrs={"id": "wob_tm"}).text,
+              'dayhour': soup.find("div", attrs={"id": "wob_dts"}).text,
+              'weather_now': soup.find("span", attrs={"id": "wob_dc"}).text,
+              'precipitation': soup.find("span", attrs={"id": "wob_pp"}).text,
+              'humidity': soup.find("span", attrs={"id": "wob_hm"}).text,
+              'wind': soup.find("span", attrs={"id": "wob_ws"}).text}
+
     next_days = []
     days = soup.find("div", attrs={"id": "wob_dp"})
     for day in days.findAll("div", attrs={"class": "wob_df"}):
@@ -54,22 +50,22 @@ city = input("Укажите свой город : ")
 
 if __name__ == "__main__":
     URL = f"https://www.google.ru/search?q=weather+{city}"
-    import argparse
-
-    parser = argparse.ArgumentParser(description="Quick Script for Extracting Weather data using Google Weather")
-    parser.add_argument("region", nargs="?", help="""Region to get weather for, must be available region.
-                                        Default is your current location determined by your IP Address""", default="")
-    # parse arguments
-    args = parser.parse_args()
-    region = args.region
-    if region:
-        region = region.replace(" ", "+")
-        URL += f"+{region}"
+    # import argparse
+    #
+    # parser = argparse.ArgumentParser(description="Quick Script for Extracting Weather data using Google Weather")
+    # parser.add_argument("region", nargs="?", help="""Region to get weather for, must be available region.
+    #                                     Default is your current location determined by your IP Address""", default="")
+    # # parse arguments
+    # args = parser.parse_args()
+    # region = args.region
+    # if region:
+    #     region = region.replace(" ", "+")
+    #     URL += f"+{region}"
     # get data
     data = get_weather_data(URL)
     # print data
     print("Погода в городе ", data["region"])
-    print("Сейчас:", data["dayhour"])
+    print("Сейчас:", t)
     print(f"Температура сейчас: {data['temp_now']}°C")
     print("Описание:", data['weather_now'])
     print("осадки", data["precipitation"])
