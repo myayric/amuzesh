9e8012699c9d631f16f3e3fbf276c5d77b803c9e
70680 <7068082@gmail.com>
2023-01-17 15:21:42 +0300

    погода

--- 3.py
+++ 3.py
@@ -1,5 +1,83 @@
-for i in range(0, 100, 20):
-    print(i*2)
+from bs4 import BeautifulSoup as bs
+import requests
+
+USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 " \
+             "Safari/537.36 "
+# US english
+LANGUAGE = "ru-RU,ru;q=0.5"
 
 
+def get_weather_data(url):
+    session = requests.Session()
+    session.headers['User-Agent'] = USER_AGENT
+    session.headers['Accept-Language'] = LANGUAGE
+    session.headers['Content-Language'] = LANGUAGE
+    html = session.get(url)
+    # create a new soup
+    soup = bs(html.text, "html.parser")
+    # store all results on this dictionary
+    result = {}
+    # extract region
+    result['region'] = soup.find("div", attrs={"id": "wob_loc"}).text
+    # extract temperature now
+    result['temp_now'] = soup.find("span", attrs={"id": "wob_tm"}).text
+    # get the day and hour now
+    result['dayhour'] = soup.find("div", attrs={"id": "wob_dts"}).text
+    # get the actual weather
+    result['weather_now'] = soup.find("span", attrs={"id": "wob_dc"}).text
+    # get the precipitation
+    result['precipitation'] = soup.find("span", attrs={"id": "wob_pp"}).text
+    # get the % of humidity
+    result['humidity'] = soup.find("span", attrs={"id": "wob_hm"}).text
+    # extract the wind
+    result['wind'] = soup.find("span", attrs={"id": "wob_ws"}).text
+    # get next few days' weather
+    next_days = []
+    days = soup.find("div", attrs={"id": "wob_dp"})
+    for day in days.findAll("div", attrs={"class": "wob_df"}):
+        # extract the name of the day
+        day_name = day.findAll("div")[0].attrs['aria-label']
+        # get weather status for that day
+        weather = day.find("img").attrs["alt"]
+        temp = day.findAll("span", {"class": "wob_t"})
+        # maximum temparature in Celsius, use temp[1].text if you want fahrenheit
+        max_temp = temp[0].text
+        # minimum temparature in Celsius, use temp[3].text if you want fahrenheit
+        min_temp = temp[2].text
+        next_days.append({"name": day_name, "weather": weather, "max_temp": max_temp, "min_temp": min_temp})
+    # append to result
+    result['next_days'] = next_days
+    return result
 
+
+city = input("Укажите свой город : ")
+
+if __name__ == "__main__":
+    URL = f"https://www.google.ru/search?q=weather+{city}"
+    import argparse
+
+    parser = argparse.ArgumentParser(description="Quick Script for Extracting Weather data using Google Weather")
+    parser.add_argument("region", nargs="?", help="""Region to get weather for, must be available region.
+                                        Default is your current location determined by your IP Address""", default="")
+    # parse arguments
+    args = parser.parse_args()
+    region = args.region
+    if region:
+        region = region.replace(" ", "+")
+        URL += f"+{region}"
+    # get data
+    data = get_weather_data(URL)
+    # print data
+    print("Погода в городе ", data["region"])
+    print("Сейчас:", data["dayhour"])
+    print(f"Температура сейчас: {data['temp_now']}°C")
+    print("Описание:", data['weather_now'])
+    print("осадки", data["precipitation"])
+    print("Влажность:", data["humidity"])
+    print("Ветер:", data["wind"])
+    print("Следующие дни:")
+    for dayweather in data["next_days"]:
+        print("=" * 40, dayweather["name"], "=" * 40)
+        print("Описание:", dayweather["weather"])
+        print(f"Максимальная температура: {dayweather['max_temp']}°C")
+        print(f"Минимальная температура: {dayweather['min_temp']}°C")
--- guess_the_number.py
+++ guess_the_number.py
@@ -69,4 +69,4 @@ async def refresh_msg(nickname, msg_box):
 
 
 if __name__ == "__main__":
-    start_server(main, debug=True, port=8080, cdn=False)
+    start_server(main, debug=True, port=8088, cdn=False)
